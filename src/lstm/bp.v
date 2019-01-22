a88eceebc1b16f883d36c4a469e1aad834dbe8c7
Yashael Faith <yashael.faith@gmail.com>
2019-01-23 01:23:26 +0700

    edited bp

--- src/lstm/bp.v
+++ src/lstm/bp.v
@@ -17,7 +17,25 @@ module bp ( clk, rst, i_layr1_a, i_layr1_i, i_layr1_f, i_layr1_o, i_layr1_state,
             i_layr1_ba, i_layr1_bi, i_layr1_bf, i_layr1_bo,
             i_layr2_wa, i_layr2_wi, i_layr2_wf, i_layr2_wo,
             i_layr2_ua, i_layr2_ui, i_layr2_uf, i_layr2_uo,
-            i_layr2_ba, i_layr2_bi, i_layr2_bf, i_layr2_bo);
+            i_layr2_ba, i_layr2_bi, i_layr2_bf, i_layr2_bo,
+            sel_layr1_in1, sel_layr1_in2, sel_layr1_in3, sel_layr1_in4, sel_layr1_in5,
+            sel_layr2_in1, sel_layr2_in2, sel_layr2_in3, sel_layr2_in4, sel_layr2_in5,
+            sel_layr1_x1_1, sel_layr1_x1_2, sel_layr1_x2_2, sel_layr1_as_1, sel_layr1_as_2, 
+            sel_layr2_x1_1, sel_layr2_x1_2, sel_layr2_x2_2, sel_layr2_as_1, sel_layr2_as_2,
+            sel_layr1_addsub, sel_layr2_addsub, sel_layr1_temp, sel_layr2_temp,
+            acc_da1, acc_di1, acc_df1, acc_do1, 
+            acc_da2, acc_di2, acc_df2, acc_do2,
+            acc_mac1, acc_mac2,
+            wr_da1, wr_di1, wr_df1, wr_do1,
+            wr_da2, wr_di2, wr_df2, wr_do2,
+            wr_dx2, wr_dout2, wr_dout1,
+            rd_addr_da1, rd_addr_di1, rd_addr_df1, rd_addr_do1,
+            wr_addr_da1, wr_addr_di1, wr_addr_df1, wr_addr_do1,
+            rd_addr_da2, rd_addr_di2, rd_addr_df2, rd_addr_do2,
+            wr_addr_da2, wr_addr_di2, wr_addr_df2, wr_addr_do2,
+            rd_addr_dx2, rd_addr_dout2, rd_addr_dout1,
+            wr_addr_dx2, wr_addr_dout2, wr_addr_dout1,
+            );
 
 // parameters
 parameter WIDTH = 32;
@@ -32,19 +50,22 @@ parameter LAYR1_dA = "layer1_dA.list";
 parameter LAYR1_dI = "layer1_dI.list";
 parameter LAYR1_dF = "layer1_dF.list";
 parameter LAYR1_dO = "layer1_dO.list";
+parameter LAYR1_dOut = "layer1_dOut.list";
 
 // This holds d gates
 parameter LAYR2_dA = "layer2_dA.list";
 parameter LAYR2_dI = "layer2_dI.list";
 parameter LAYR2_dF = "layer2_dF.list";
 parameter LAYR2_dO = "layer2_dO.list";
+parameter LAYR2_dX = "layer2_dX.list";
+parameter LAYR2_dOut = "layer2_dOut.list";
 
 // common ports
 input clk, rst;
 
 // input ports
-input signed [WIDTH-1:0] i_layr1_a, i_layr1_i, i_layr1_f, i_layr1_o, i_layr1_state, i_layr1_d_state;
-input signed [WIDTH-1:0] i_layr2_a, i_layr2_i, i_layr2_f, i_layr2_o, i_layr2_state, i_layr2_d_state, i_layr2_h, i_layr2_t;
+input signed [WIDTH-1:0] i_layr1_a, i_layr1_i, i_layr1_f, i_layr1_o, i_layr1_state;
+input signed [WIDTH-1:0] i_layr2_a, i_layr2_i, i_layr2_f, i_layr2_o, i_layr2_state, i_layr2_h, i_layr2_t;
 
 input signed [WIDTH-1:0] i_layr1_wa, i_layr1_wi, i_layr1_wf, i_layr1_wo;
 input signed [WIDTH-1:0] i_layr1_ua, i_layr1_ui, i_layr1_uf, i_layr1_uo;
@@ -54,49 +75,54 @@ input signed [WIDTH-1:0] i_layr2_ua, i_layr2_ui, i_layr2_uf, i_layr2_uo;
 input signed [WIDTH-1:0] i_layr2_ba, i_layr2_bi, i_layr2_bf, i_layr2_bo;
 
 // control ports
-input sel_layr1_in1;
-input sel_layr1_in2;
-input sel_layr1_in3;
-input sel_layr1_in4;
-input sel_layr1_in5;
-input sel_layr1_x1_1;
-input sel_layr1_x1_2;
-input sel_layr1_x2_2;
-input sel_layr1_as_1;
-input sel_layr1_as_2;
-input sel_layr1_addsub;
-input sel_layr1_temp;
+input sel_layr1_in1, sel_layr2_in1;
+input sel_layr1_in2, sel_layr2_in2;
+input sel_layr1_in3, sel_layr2_in3;
+input sel_layr1_in4, sel_layr2_in4;
+input sel_layr1_in5, sel_layr2_in5;
+input sel_layr1_x1_1, sel_layr2_x1_1;
+input sel_layr1_x1_2, sel_layr2_x1_2;
+input sel_layr1_x2_2, sel_layr2_x2_2;
+input sel_layr1_as_1, sel_layr2_as_1;
+input sel_layr1_as_2, sel_layr2_as_2;
+input sel_layr1_addsub, sel_layr2_addsub;
+input sel_layr1_temp, sel_layr2_temp;
 
-input sel_layr2_in1;
-input sel_layr2_in2;
-input sel_layr2_in3;
-input sel_layr2_in4;
-input sel_layr2_in5;
-input sel_layr2_x1_1;
-input sel_layr2_x1_2;
-input sel_layr2_x2_2;
-input sel_layr2_as_1;
-input sel_layr2_as_2;
-input sel_layr2_addsub;
-input sel_layr2_temp;
+input acc_da1, acc_di1, acc_df1, acc_do1;
+input acc_da2, acc_di2, acc_df2, acc_do2;
+input acc_mac1, acc_mac2;
 
+input [WIDTH-1:0] wr_da1, wr_di1, wr_df1, wr_do1;
+input [WIDTH-1:0] rd_addr_da1, rd_addr_di1, rd_addr_df1, rd_addr_do1;
+input [WIDTH-1:0] wr_addr_da1, wr_addr_di1, wr_addr_df1, wr_addr_do1;
+
+input [WIDTH-1:0] wr_da2, wr_di2, wr_df2, wr_do2;
+input [WIDTH-1:0] rd_addr_da2, rd_addr_di2, rd_addr_df2, rd_addr_do2;
+input [WIDTH-1:0] wr_addr_da2, wr_addr_di2, wr_addr_df2, wr_addr_do2;
+
+input [WIDTH-1:0] wr_dx2, wr_dout2, wr_dout1;
+input [WIDTH-1:0] rd_addr_dx2, rd_addr_dout2, rd_addr_dout1;
+input [WIDTH-1:0] wr_addr_dx2, wr_addr_dout2, wr_addr_dout1;
 // output ports
 
 
 // registers
+reg signed [WIDTH-1:0] reg_d_state1, reg_d_state2;
 
 
 // wires
-wire signed [WIDTH-1:0] wr_da2, wr_di2, wr_df2, wr_do2;
-wire signed [WIDTH-1:0] rd_addr_da2, rd_addr_di2, rd_addr_df2, rd_addr_do2;
-wire signed [WIDTH-1:0] wr_addr_da2, wr_addr_di2, wr_addr_df2, wr_addr_do2;
-
-wire signed [WIDTH-1:0] wr_da1, wr_di1, wr_df1, wr_do1;
-wire signed [WIDTH-1:0] rd_addr_da1, rd_addr_di1, rd_addr_df1, rd_addr_do1;
-wire signed [WIDTH-1:0] wr_addr_da1, wr_addr_di1, wr_addr_df1, wr_addr_do1;
-
+wire signed [WIDTH-1:0] layr1_d_state, layr2_d_state;
+wire signed [WIDTH-1:0] dgate_layr1, dgate_layr2;
 wire signed [WIDTH-1:0] da1, di1, df1, do1, da2, di2, df2, do2;
 
+wire signed [WIDTH-1:0] o_acc_da1, o_acc_di1, o_acc_df1, o_acc_do1;
+wire signed [WIDTH-1:0] o_acc_da2, o_acc_di2, o_acc_df2, o_acc_do2;
+
+wire signed [WIDTH-1:0] dgate_mux1, dgate_mux2;
+wire signed [WIDTH-1:0] wghts_mux1, wghts_mux2;
+wire signed [WIDTH-1:0] o_mac1, o_mac2;
+
+wire signed [WIDTH-1:0] dx2, dout1, dout2;
 
 // LAYER 2 Delta
 // in: i_layr2_a, i_layr2_i, i_layr2_f, i_layr2_o, i_layr2_state, i_layr2_h, i_layr2_t
@@ -126,96 +152,161 @@ delta #(
         .h          (),
         .t          (),
         .state      (i_layr2_state),
-        .d_state    (i_layr2_d_state),
+        .d_state    (reg_d_state2),
         .d_out      (),
         .o_dgate    (dgate_layr2)
     );
 
+// LAYER 2 delta State Delay Register
+always @(posedge clk)
+begin
+    reg_d_state2 = layr2_d_state;
+end
+
 
 // LAYER 2 dA, dI, dF, dO Memory
 // out: da2, di2, df2, do2
-memory_dgates #(
+memory_cell #(
+        .ADDR(6),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR2_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(56),
+        .TIMESTEP(1),
         .FILENAME(LAYR2_dA)
     ) mem_da2 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_da2),
-        .rd_addr (rd_addr_da2),
-        .wr_addr (wr_addr_da2),
-        .i       (dgate_layr2),
-        .o       (da2)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_da2),
+        .addr_a (wr_addr_da2),
+        .addr_b (rd_addr_da2),
+        .i_a    (dgate_layr2),
+        .o_a    (),
+        .o_b    (da2)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(6),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR2_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(56),
+        .TIMESTEP(1),
         .FILENAME(LAYR2_dI)
     ) mem_di2 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_di2),
-        .rd_addr (rd_addr_di2),
-        .wr_addr (wr_addr_di2),
-        .i       (dgate_layr2),
-        .o       (di2)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_di2),
+        .addr_a (wr_addr_di2),
+        .addr_b (rd_addr_di2),
+        .i_a    (dgate_layr2),
+        .o_a    (),
+        .o_b    (di2)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(6),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR2_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(56),
+        .TIMESTEP(1),
         .FILENAME(LAYR2_dF)
     ) mem_df2 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_df2),
-        .rd_addr (rd_addr_df2),
-        .wr_addr (wr_addr_df2),
-        .i       (dgate_layr2),
-        .o       (df2)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_df2),
+        .addr_a (wr_addr_df2),
+        .addr_b (rd_addr_df2),
+        .i_a    (dgate_layr2),
+        .o_a    (),
+        .o_b    (df2)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(6),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR2_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(56),
+        .TIMESTEP(1),
         .FILENAME(LAYR2_dO)
     ) mem_do2 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_do2),
-        .rd_addr (rd_addr_do2),
-        .wr_addr (wr_addr_do2),
-        .i       (dgate_layr2),
-        .o       (do2)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_do2),
+        .addr_a (wr_addr_do2),
+        .addr_b (rd_addr_do2),
+        .i_a    (dgate_layr2),
+        .o_a    (),
+        .o_b    (do2)
     );
+
 
 // LAYER 2 ACC Array for dX and delta Out
 // in: da, di, df, do
 // out: dx, delta Out
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_da_2 (.clk(clk), .rst(rst), .acc(acc_da2), .i(dgate_layr2), .o(o_acc_da2));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_di_2 (.clk(clk), .rst(rst), .acc(acc_di2), .i(dgate_layr2), .o(o_acc_di2));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_df_2 (.clk(clk), .rst(rst), .acc(acc_df2), .i(dgate_layr2), .o(o_acc_df2));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_do_2 (.clk(clk), .rst(rst), .acc(acc_do2), .i(dgate_layr2), .o(o_acc_do2));
+
+// LAYER 2 dgates Multiplexer
+// in: o_acc_da2, o_acc_di2, o_acc_df2, o_acc_do2
+// out: dgate_mux2
 
 
-// LAYER 2 Multiplexer
-// in: dgates memory
-// out: da/di/df/do
+// LAYER 2 W & U Memory Multiplexer
+// in: i_layr2_wa, i_layr2_wi, i_layr2_wf, i_layr2_wo,i_layr2_ua, i_layr2_ui, i_layr2_uf, i_layr2_uo
+// out: wghts_mux2
 
 
 // LAYER 2 MAC
 // in: dgate
 // out: dX/delta Out
-
+mac #(
+        .WIDTH(WIDTH),
+        .FRAC(FRAC)
+    ) mac2 (
+        .clk (clk),
+        .rst (rst),
+        .acc (acc_mac2),
+        .i_x (dgate_mux2),
+        .i_m (wghts_mux2),
+        .o   (o_mac2)
+    );
 
 // LAYER 2 dX Memory
-// in: acc_o
-
+// in: o_mac2
+// out: dx2
+memory_cell #(
+        .ADDR(9),
+        .WIDTH(WIDTH),
+        .NUM(371),
+        .TIMESTEP(1),
+        .FILENAME(LAYR2_dX)
+    ) mem_dx2 (
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_dx2),
+        .addr_a (wr_addr_dx2),
+        .addr_b (rd_addr_dx2),
+        .i_a    (o_mac2),
+        .o_a    (),
+        .o_b    (dx2)
+    );
 
 // LAYER 2 delta Out Memory
-// in: acc_o
-
+// in: o_mac2
+// out: dout2
+memory_cell #(
+        .ADDR(4),
+        .WIDTH(WIDTH),
+        .NUM(16),
+        .TIMESTEP(1),
+        .FILENAME(LAYR2_dOut)
+    ) mem_dout2 (
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_dout2),
+        .addr_a (wr_addr_dout2),
+        .addr_b (rd_addr_dout2),
+        .i_a    (o_mac2),
+        .o_a    (),
+        .o_b    (dout2)
+    );
 
 // LAYER 1 Delta
 // in: i_layr1_a, i_layr1_i, i_layr1_f, i_layr1_o, i_layr1_state;
@@ -245,93 +336,142 @@ delta #(
         .h          (),
         .t          (),
         .state      (i_layr1_state),
-        .d_state    (i_layr1_d_state),
+        .d_state    (),
         .d_out      (),
         .o_dgate    (dgate_layr1)
     );
 
+// LAYER 2 delta State Delay Register
+always @(posedge clk)
+begin
+    reg_d_state1 = layr1_d_state;
+end
+
 // LAYER 1 dA, dI, dF, dO Memory
 // in: addr, wr, odgate
 // out: da, di, df, do
-memory_dgates #(
+memory_cell #(
+        .ADDR(9),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR1_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(371),
+        .TIMESTEP(1),
         .FILENAME(LAYR1_dA)
     ) mem_da1 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_da1),
-        .rd_addr (rd_addr_da1),
-        .wr_addr (wr_addr_da1),
-        .i       (dgate_layr1),
-        .o       (da1)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_da1),
+        .addr_a (wr_addr_da1),
+        .addr_b (rd_addr_da1),
+        .i_a    (dgate_layr1),
+        .o_a    (),
+        .o_b    (da1)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(9),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR1_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(371),
+        .TIMESTEP(1),
         .FILENAME(LAYR1_dI)
     ) mem_di1 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_di1),
-        .rd_addr (rd_addr_di1),
-        .wr_addr (wr_addr_di1),
-        .i       (dgate_layr1),
-        .o       (di1)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_di1),
+        .addr_a (wr_addr_di1),
+        .addr_b (rd_addr_di1),
+        .i_a    (dgate_layr1),
+        .o_a    (),
+        .o_b    (di1)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(9),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR1_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(371),
+        .TIMESTEP(1),
         .FILENAME(LAYR1_dF)
     ) mem_df1 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_df1),
-        .rd_addr (rd_addr_df1),
-        .wr_addr (wr_addr_df1),
-        .i       (dgate_layr1),
-        .o       (df1)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_df1),
+        .addr_a (wr_addr_df1),
+        .addr_b (rd_addr_df1),
+        .i_a    (dgate_layr1),
+        .o_a    (),
+        .o_b    (df1)
     );
 
-memory_dgates #(
+memory_cell #(
+        .ADDR(9),
         .WIDTH(WIDTH),
-        .NUM_LSTM(LAYR1_CELL),
-        .TIMESTEP(TIMESTEP),
+        .NUM(371),
+        .TIMESTEP(1),
         .FILENAME(LAYR1_dO)
     ) mem_do1 (
-        .clk     (clk),
-        .rst     (rst),
-        .wr      (wr_do1),
-        .rd_addr (rd_addr_do1),
-        .wr_addr (wr_addr_do1),
-        .i       (dgate_layr1),
-        .o       (do1)
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_do1),
+        .addr_a (wr_addr_do1),
+        .addr_b (rd_addr_do1),
+        .i_a    (dgate_layr1),
+        .o_a    (),
+        .o_b    (do1)
     );
-
 
 // LAYER 1 ACC Array
 // in: dgates memory
 // out: da/di/df/do
+// input acc_da1, acc_di1, acc_df1, acc_do1;
+// wire signed [WIDTH-1:0] o_acc_da1, o_acc_di1, o_acc_df1, o_acc_do1;
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_da_1 (.clk(clk), .rst(rst), .acc(acc_da1), .i(dgate_layr1), .o(o_acc_da1));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_di_1 (.clk(clk), .rst(rst), .acc(acc_di1), .i(dgate_layr1), .o(o_acc_di1));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_df_1 (.clk(clk), .rst(rst), .acc(acc_df1), .i(dgate_layr1), .o(o_acc_df1));
+acc #(.WIDTH(WIDTH), .FRAC(FRAC)) acc_do_1 (.clk(clk), .rst(rst), .acc(acc_do1), .i(dgate_layr1), .o(o_acc_do1));
 
 
-// LAYER 1 Multiplexer
-// in: dgates memory
-// out: da/di/df/do
+// LAYER 1 dgates Multiplexer
+// in: o_acc_da1, o_acc_di1, o_acc_df1, o_acc_do1
+// out: dgate_mux1
+
+
+// LAYER 1 U Memory Multiplexer
+// in: i_layr1_wa, i_layr1_wi, i_layr1_wf, i_layr1_wo,i_layr1_ua, i_layr1_ui, i_layr1_uf, i_layr1_uo
+// out: wghts_mux1
 
 
 // LAYER 1 MAC
 // in: dgate
 // out: dX/delta Out
-
+mac #(
+        .WIDTH(WIDTH),
+        .FRAC(FRAC)
+    ) mac1 (
+        .clk (clk),
+        .rst (rst),
+        .acc (acc_mac1),
+        .i_x (dgate_mux1),
+        .i_m (wghts_mux1),
+        .o   (o_mac1)
+    );
 
 // LAYER 1 delta Out Memory
-// in: delta Out
-// out: reg delta Out
-
+// in: o_mac1
+// out: dout1
+memory_cell #(
+        .ADDR(7),
+        .WIDTH(WIDTH),
+        .NUM(106),
+        .TIMESTEP(1),
+        .FILENAME(LAYR1_dOut)
+    ) mem_dout1 (
+        .clk    (clk),
+        .rst    (rst),
+        .wr_a   (wr_dout1),
+        .addr_a (wr_addr_dout1),
+        .addr_b (rd_addr_dout1),
+        .i_a    (o_mac1),
+        .o_a    (),
+        .o_b    (dout1)
+    );
 
 endmodule
--- src/lstm/memory_cell.v
+++ src/lstm/memory_cell.v
@@ -10,9 +10,10 @@
 //                    
 //            
 ///////////////////////////////////////////////////////////////////////////////
-module memory_cell(clk, rst, wr_a, addr_a, addr_b, i_a, i_b, o_a, o_b);
+module memory_cell(clk, rst, wr_a, addr_a, addr_b, i_a, o_a, o_b);
 
 // parameters
+parameter ADDR = 12;
 parameter WIDTH = 32;
 parameter NUM = 53*53;
 parameter TIMESTEP = 1;
@@ -22,11 +23,9 @@ parameter FILENAME = "layer_act.list";
 input clk, rst, wr_a;
 
 // control ports
-input [11:0] addr_a;
-input [11:0] addr_b;
+input [ADDR-1:0] addr_a;
+input [ADDR-1:0] addr_b;
 input [WIDTH-1:0] i_a;
-input [WIDTH-1:0] i_b;
-
 
 // output ports
 output reg signed [WIDTH-1:0] o_a;
